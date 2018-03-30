2f84af5b0071cdac5b2e60be933e67efd70df749
Nicholas-Thompson-1996 <nick.tomo99@yahoo.com>
2018-03-30 16:29:40 +0100

    Update Check

--- group19/src/comp207p/main/ConstantFolder.java
+++ group19/src/comp207p/main/ConstantFolder.java
@@ -38,13 +38,75 @@ public class ConstantFolder
 		}
 	}
 	
+	private void optimizeMethod(ClassGen cgen, ConstantPoolGen cpgen, Method method)
+	{
+		//Get the code of method - collection of byte code
+		Code methodCode = method.getCode();
+		
+		//Now get actual bytecode data in bytearray
+		//Initialise instruction List
+		InstructionList instList = new InstructionList(methodCode.getCOde());
+
+		// Initialise a method generator with the original method as the baseline	
+		MethodGen methodGen = new MethodGen(method.getAccessFlags(), method.getReturnType(), method.getArgumentTypes(), null, method.getName(), cgen.getClassName(), instList, cpgen);
+
+		// InstructionHandle is a wrapper for actual Instructions
+		for (InstructionHandle handle : instList.getInstructionHandles())
+		{
+			// if the instruction inside is iconst
+			if (handle.getInstruction() instanceof ICONST)
+			{
+				// insert new one with integer 5, and...
+				instList.insert(handle, new ICONST(5));
+				try
+				{
+					// delete the old one
+					instList.delete(handle);
+
+				}
+				catch (TargetLostException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// setPositions(true) checks whether jump handles 
+		// are all within the current method
+		instList.setPositions(true);
+
+		// set max stack/local
+		methodGen.setMaxStack();
+		methodGen.setMaxLocals();
+
+		// generate the new method with replaced iconst
+		Method newMethod = methodGen.getMethod();
+		// replace the method in the original class
+		cgen.replaceMethod(method, newMethod);
+
+	}
+	
+	
 	public void optimize()
 	{
+		//loads original class into a class generator
 		ClassGen cgen = new ClassGen(original);
 		ConstantPoolGen cpgen = cgen.getConstantPool();
 
 		// Implement your optimization here
-        
+		
+		//get the current constant pool
+        	ConstantPool cp = cpgen.getConstantPool();
+		// get the constants in the pool
+		Constant[] constants = cp.getConstantPool();
+		
+		Method[] methods = cgen.getMethods();
+		for(Method m : methods)
+		{
+			optimizeMethod(cgen, cpgen, m);
+		}
+		
 		this.optimized = gen.getJavaClass();
 	}
 
